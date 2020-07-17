5750159e8c8264fa6bc97a32f767abb306d8d672
Gatxy <63024945+gatxy@users.noreply.github.com>
2020-07-18 00:34:43 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,236 @@
+import sys
+import os
+import json
+
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout,
+                             QPushButton, QLabel, QLineEdit, QTabBar,
+                             QFrame, QStackedLayout, QTabWidget, QShortcut, QKeySequenceEdit,
+                             QSplitter)
+
+from PyQt5.QtGui import QIcon, QWindow, QImage, QKeySequence
+from PyQt5.QtCore import *
+from PyQt5.QtWebEngineWidgets import *
+
+
+class AddressBar(QLineEdit):
+    def __init__(self):
+        super().__init__()
+
+    def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
+        self.selectAll()
+
+
+class App(QFrame):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Web Browser")
+
+        self.setBaseSize(1366, 768)
+        self.setMinimumSize(1366, 768)
+        self.CreateApp()
+        self.setWindowIcon(QIcon("bootstrap-stack.png"))
+
+    def CreateApp(self):
+        self.layout = QVBoxLayout()
+        self.layout.setSpacing(0)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+
+        # Create Tabs
+        self.tabbar = QTabBar(movable=True, tabsClosable=True)
+        self.tabbar.tabCloseRequested.connect(self.CloseTab)
+        self.tabbar.tabBarClicked.connect(self.SwitchTab)
+        self.tabbar.setCurrentIndex(0)
+        self.tabbar.setDrawBase(False)
+        self.tabbar.setLayoutDirection(Qt.LeftToRight)
+        self.tabbar.setElideMode(Qt.ElideLeft)
+
+        self.shortcutNewTab = QShortcut(QKeySequence("Ctrl+T"), self)
+        self.shortcutNewTab.activated.connect(self.AddTab)
+
+        self.shortcutReload = QShortcut(QKeySequence("Ctrl+R"), self)
+        self.shortcutReload.activated.connect(self.ReloadPage)
+
+        # Keep Track of Tabs
+        self.tabCount = 0
+        self.tabs = []
+
+        # Create AddressBar
+        self.Toolbar = QWidget()
+        self.Toolbar.setObjectName("Toolbar")
+        self.ToolbarLayout = QHBoxLayout()
+        self.addressbar = AddressBar()
+        self.AddTabButton = QPushButton("+")
+
+        # Connect AddressBar + button signals
+        self.addressbar.returnPressed.connect(self.BrowseTo)
+        self.AddTabButton.clicked.connect(self.AddTab)
+
+        # Set Toolbar Buttons
+        self.BackButton = QPushButton("<")
+        self.BackButton.clicked.connect(self.GoBack)
+
+        self.ForwardButton = QPushButton(">")
+        self.ForwardButton.clicked.connect(self.GoForward)
+
+        self.ReloadButton = QPushButton("R")
+        self.ReloadButton.clicked.connect(self.ReloadPage)
+
+        # Build Toolbar
+        self.Toolbar.setLayout(self.ToolbarLayout)
+        self.ToolbarLayout.addWidget(self.BackButton)
+        self.ToolbarLayout.addWidget(self.ForwardButton)
+        self.ToolbarLayout.addWidget(self.ReloadButton)
+        self.ToolbarLayout.addWidget(self.addressbar)
+        self.ToolbarLayout.addWidget(self.AddTabButton)
+
+        # Set Main View
+        self.container = QWidget()
+        self.container.layout = QStackedLayout()
+        self.container.setLayout(self.container.layout)
+
+        self.layout.addWidget(self.tabbar)
+        self.layout.addWidget(self.Toolbar)
+        self.layout.addWidget(self.container)
+
+        self.setLayout(self.layout)
+
+        self.AddTab()
+
+        self.show()
+
+    def CloseTab(self, i):
+        self.tabbar.removeTab(i)
+
+    def AddTab(self):
+        i = self.tabCount
+
+        # set self.tabs<#> = QWidget
+        self.tabs.append(QWidget())
+        self.tabs[i].layout = QVBoxLayout()   # Access's QWidget
+        self.tabs[i].layout.setContentsMargins(0, 0, 0, 0)
+
+
+        # For tab switching
+        self.tabs[i].setObjectName("tab" + str(i))
+
+        # Create webview within the tabs top level widget
+        self.tabs[i].content = QWebEngineView()
+        self.tabs[i].content.load(QUrl.fromUserInput("https://www.google.com/"))
+
+        self.tabs[i].content.titleChanged.connect(lambda: self.SetTabContent(i, "title"))
+        self.tabs[i].content.iconChanged.connect(lambda: self.SetTabContent(i, "icon"))
+        self.tabs[i].content.urlChanged.connect(lambda: self.SetTabContent(i, "url"))
+
+        # Add widget to tab.layout
+        self.tabs[i].layout.addWidget(self.tabs[i].content)
+
+        # Set tab.layout to .layout
+        self.tabs[i].setLayout(self.tabs[i].layout)
+
+        # Add and set new tabs content to the stack widget
+        self.container.layout.addWidget(self.tabs[i])
+        self.container.layout.setCurrentWidget(self.tabs[i])
+
+        # Create tab on tabbar, representing this tab,
+        # Set tabData to tab<#> So it knows what self.tabs[#] it needs to control
+        self.tabbar.addTab("New Tab")
+        self.tabbar.setTabData(i, {"object": "tab" + str(i), "initial": i})
+
+        self.tabbar.setCurrentIndex(i)
+
+        self.tabCount += 1
+
+    def SwitchTab(self, i):
+        # I = self.tabbar.currentIndex()
+        if self.tabbar.tabData(i):
+            tab_data = self.tabbar.tabData(i)["object"]
+            tab_content = self.findChild(QWidget, tab_data)
+            self.container.layout.setCurrentWidget(tab_content)
+            new_url = tab_content.content.url().toString()
+            self.addressbar.setText(new_url)
+
+    def BrowseTo(self):
+        text = self.addressbar.text()
+        print(text)
+
+        i = self.tabbar.currentIndex()              # Get index of current tab
+        tab = self.tabbar.tabData(i)["object"]      # Get tab name of the name
+        wv = self.findChild(QWidget, tab).content   # Get webview associated with the name
+
+        if "http" not in text:
+            if "." not in text:
+                url = "https://www.google.com/#q=" + text
+            else:
+                url = "http://" + text
+        else:
+            url = text
+
+        wv.load(QUrl.fromUserInput(text))
+
+    def SetTabContent(self, i, type):
+        '''
+            self.tabs[i].objectName = tab1
+            self.tabbar.tabData(i)["object] = tab1
+        '''
+        tab_name = self.tabs[i].objectName()
+        # tab1
+
+        count = 0
+        running = True
+
+        current_tab = self.tabbar.tabData(self.tabbar.currentIndex())["object"]
+
+        if current_tab == tab_name and type == "url":
+            new_url = self.findChild(QWidget, tab_name).content.url().toString()
+            self.addressbar.setText(new_url)
+            return False
+
+        while running:
+            tab_data_name = self.tabbar.tabData(count)
+
+            if count >= 99:
+                running = False
+
+            if tab_name == tab_data_name["object"]:
+                if type == "title":
+                    newTitle = self.findChild(QWidget, tab_name).content.title()
+                    self.tabbar.setTabText(count, newTitle)
+                elif type == "icon":
+                    newIcon = self.findChild(QWidget, tab_name).content.icon()
+                    self.tabbar.setTabIcon(count, newIcon)
+
+                running = False
+            else:
+                count += 1
+
+    def GoBack(self):
+        activeIndex = self.tabbar.currentIndex()
+        tab_name = self.tabbar.tabData(activeIndex)["object"]
+        tab_content = self.findChild(QWidget, tab_name).content
+
+        tab_content.back()
+
+    def GoForward(self):
+        activeIndex = self.tabbar.currentIndex()
+        tab_name = self.tabbar.tabData(activeIndex)["object"]
+        tab_content = self.findChild(QWidget, tab_name).content
+
+        tab_content.forward()
+
+    def ReloadPage(self):
+        activeIndex = self.tabbar.currentIndex()
+        tab_name = self.tabbar.tabData(activeIndex)["object"]
+        tab_content = self.findChild(QWidget, tab_name).content
+
+        tab_content.reload()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = App()
+
+    with open("style.css", "r") as style:
+        app.setStyleSheet(style.read())
+
+    sys.exit(app.exec_())
